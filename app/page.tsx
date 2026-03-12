42c57cedb367338a29849d59c126f7411bb0189e
dannycottonhat-crypto <dannycottonhat@gmail.com>
2026-03-12 12:32:43 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{padding:40,fontFamily:"sans-serif"}}>
+      <h1>Panzzoni</h1>
+      <p>Cantina e Pizza</p>
+      <p>Tradizione Italiana · Forno a Legna</p>
+    </main>
+  )
+}

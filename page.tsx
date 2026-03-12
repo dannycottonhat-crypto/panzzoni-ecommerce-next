6b3f3f505decbc25eb5aba53ebdd294ea78b5ab5
dannycottonhat-crypto <dannycottonhat@gmail.com>
2026-03-12 12:44:15 +0100

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding:40,fontFamily:"sans-serif"}}>
-      <h1>Panzzoni</h1>
-      <p>Cantina e Pizza</p>
-      <p>Tradizione Italiana · Forno a Legna</p>
-    </main>
-  )
-}
